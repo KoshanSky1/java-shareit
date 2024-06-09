346cfeea7cdd192d3ee80c053f00db1785d49837
Galina Lobacheva <147919859+KoshanSky1@users.noreply.github.com>
2024-06-09 08:10:16 +0300

    fix: исправить Comment

--- src/main/java/ru/practicum/shareit/item/model/Comment.java
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.shareit.user.model.User;
 
-import javax.persistence.*;;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
